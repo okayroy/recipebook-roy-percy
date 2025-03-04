d7b7a7832bd9cdd5be6ffb5dbf6f60a21a080f31
bean juice <sarahXD@Ralph-Roys-MacBook-Air-2.local>
2025-03-04 23:58:04 +0800

    Edited view names in urls.py

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,14 +1,14 @@
 """This file defines the url path for each view"""
 
 from django.urls import path
-from .views import index, recipelist, recipe1, recipe2
+from .views import index, recipes_view, recipe1_view, recipe2_view
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('recipes/list', recipelist, name='recipelist'),
-    path('recipe/1', recipe1, name="recipe1"),
-    path('recipe/2', recipe2, name="recipe2")
+    path('recipes/list', recipes_view, name='recipelist'),
+    path('recipe/1', recipe1_view, name="recipe1"),
+    path('recipe/2', recipe2_view, name="recipe2")
     ]
 
 app_name = "ledger"
