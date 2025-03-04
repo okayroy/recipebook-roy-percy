075af1957411c0914c4496559c6292b1d789cb0c
bean juice <sarahXD@Ralph-Roys-MacBook-Air-2.local>
2025-03-04 23:10:00 +0800

    Edited templates folder and urls.py from lab1

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+"""This file defines the url path for each view"""
 
-from .views import index
+from django.urls import path
+from .views import index, recipelist, recipe1, recipe2
+
 
 urlpatterns = [
     path('', index, name='index'),
-]
+    path('recipes/list', recipelist, name='recipelist'),
+    path('recipe/1', recipe1, name="recipe1"),
+    path('recipe/2', recipe2, name="recipe2")
+    ]
 
 app_name = "ledger"
