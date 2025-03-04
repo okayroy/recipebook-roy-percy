8128abb9b2a2308fc75ab55ef2029730bad7c3fe
bean juice <sarahXD@Ralph-Roys-MacBook-Air-2.local>
2025-03-04 16:44:32 +0800

    Updating models.py for lab2

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,57 @@
-from django.db import models
+"""This file provides the base models for python."""
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+class Ingredient(models.Model):
+    """This class represents the ingredients in a recipe."""
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+    """Returns name of the ingredient."""
+
+        return self.name
+    
+    def get_absolute_url(self):
+    """Returns absolute url of the ingredients."""
+
+        return reverse('ingredient_detail', args=[str(self.name)])
+
+
+class Recipe(models.Model):
+    """This class represents the recipes itself."""
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+    """Returns recipe name."""
+
+        return self.name
+    
+    def get_absolute_url(self):
+    """Returns absolute url of the recipe."""
+
+        return reverse('recipe_detail', args=[str(self.name)])
+
+
+class RecipeIngredient(models.Model):
+    """This class specifies quantity, and uses foreign keys to refer
+    back to previously established recipes and ingredients."""
+    
+    quantity = models.CharField(max_length=100)
+    
+    ingredient - models.ForeignKey(
+        Ingredient,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='recipe',
+    )
+    
+    recipe - models.ForeignKey(
+        Recipe,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='ingredients',
+    )
