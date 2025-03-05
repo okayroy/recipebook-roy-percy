51f178a48a017190c50d2b5f4588901f08f42883
okayroy <160197662+okayroy@users.noreply.github.com>
2025-03-05 11:17:07 +0800

    Updated admin.py to include a ModelAdmin and InlineAdmin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,20 @@
+"""This file sets up the admin panel."""
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient
 
-# Register your models here.
+
+class RecipeInLine(admin.TabularInLine):
+    """Creates InlineAdmin of RecipeIngredient."""
+
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    """Admin panel for the Recipe model."""
+
+    inlines = [RecipeInLine, ]
+    model = Recipe
+    search_fields = ('name', )
+
+
+admin.site.register(Recipe, RecipeAdmin)
