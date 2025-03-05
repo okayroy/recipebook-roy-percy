a2f3930736ad60a46679580a7efbb2352fa5b8fe
okayroy <160197662+okayroy@users.noreply.github.com>
2025-03-05 10:50:18 +0800

    Updated urls.py paths to include list + detail views

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,14 +1,12 @@
-"""This file defines the url path for each view"""
-
+"""This file defines the url path for each view."""
 from django.urls import path
-from .views import index, recipes_view, recipe1_view, recipe2_view
+from .views import index, RecipeListView, RecipeDetailView
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('recipes/list', recipes_view, name='recipelist'),
-    path('recipe/1', recipe1_view, name="recipe1"),
-    path('recipe/2', recipe2_view, name="recipe2")
+    path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
+    path('recipe/<int:id>', RecipeDetailView.as_view(), name='recipe-detail'),
     ]
 
 app_name = "ledger"
